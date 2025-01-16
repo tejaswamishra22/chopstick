4b79e5a284e8064be543ab9f24922734ac1fb484
Tejaswa Mishra <tejaswamishra22@gmail.com>
2025-01-16 21:52:13 +0530

    started adding decision tree for hard levels

--- logic/hello.py
+++ logic/hello.py
@@ -1,23 +1,29 @@
-import numpy as np
+class Node:
+    def __init__(self, values):
+        self.values = values  # List of 5 integers
+        self.children = []
 
-def fibonacci(n, dp=None):
-    if dp is None:
-        dp = [-1] * (n + 1)
+def dfs(node, visited=None):
+    if visited is None:
+        visited = set()
     
-    if n <= 1:
-        return n
+    if node in visited:
+        return
     
-    if dp[n] != -1:
-        return dp[n]
+    visited.add(node)
+    print(node.values)
     
-    dp[n] = fibonacci(n - 1, dp) + fibonacci(n - 2, dp)
-    return dp[n]
+    for child in node.children:
+        dfs(child, visited)
 
-msg = "Roll a dice!"
-print(msg)
+# Example usage
+root = Node([1, 2, 3, 4, 5])
+child1 = Node([6, 7, 8, 9, 10])
+child2 = Node([11, 12, 13, 14, 15])
+child3 = Node([16, 17, 18, 19, 20])
 
-cache_hard = [[[[[0 for _ in range(5)] for _ in range(5)] for _ in range(5)] for _ in range(5)] for _ in range(2)]
+root.children.append(child1)
+root.children.append(child2)
+child1.children.append(child3)
 
-print(cache_hard[0][0][0][0][0])
-
-
+dfs(root)
